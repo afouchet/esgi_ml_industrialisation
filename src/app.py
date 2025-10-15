d3ba1f602b5a8f1cb1cd4cc116dcc42c8119cd1c
Arnaud Fouchet <foucheta@gmail.com>
2025-10-15 22:00:19 +0200

    Refacto: code sales is more a 'service' (doing action) than a model (that stores info)

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 import os
 
-import models
+import services
 import features
 
 PATH_CSV = "data/raw/db.csv"
@@ -17,24 +17,24 @@ def create_app(config=None):
 
     app.config.update(config)
 
-    models.sales.init_database(config)
+    services.sales.init_database(config)
 
     @app.route('/post_sales', methods=['POST'])
     def post_sales():
         data = request.json
 
-        models.sales.add_weekly_sales(data)
+        services.sales.add_weekly_sales(data)
 
         return jsonify({"status": "success"}), 200
 
     @app.route('/get_weekly_sales', methods=['GET'])
     def get_weekly_sales():
-        df = models.sales.get_weekly_sales()
+        df = services.sales.get_weekly_sales()
         return jsonify(df.to_dict(orient="records")), 200
 
     @app.route('/get_monthly_sales', methods=['GET'])
     def get_monthly_sales():
-        df_weekly = models.sales.get_weekly_sales()
+        df_weekly = services.sales.get_weekly_sales()
         df_monthly = features.compute_monthly_sales(df_weekly)
 
         return jsonify(df_monthly.to_dict(orient="records")), 200

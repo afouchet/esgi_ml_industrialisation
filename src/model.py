6b0b46280cfc27c97cdfb1fb276c63d000aad6fe
Arnaud Fouchet <foucheta@gmail.com>
2025-10-16 17:37:32 +0200

    Refacto make features & model to have Ridge

--- src/model.py
+++ src/model.py
@@ -1,14 +1,60 @@
 import pandas as pd
+from sklearn.linear_model import Ridge
 
 def make_predictions(config):
     df_sales = pd.read_csv(config["data"]["sales"])
 
+    df = make_features(df_sales, config)
 
+    df_train = df.query("dates < @config['start_test']")
+    y_train = df_train.pop("sales")
+
+    df_test = df.query("dates >= @config['start_test']")
+    y_test = df_test.pop("sales")
+
+    model = build_model(config)
+
+    model.fit(df_train, y_train)
+
+    pred = model.predict(df_test)
+
+    df_test["prediction"] = pred
+
+    return df_test["prediction"].reset_index()
+
+
+def make_features(df_sales, config):
+    df = df_sales.copy()
+
+    df["sales_last_month"] = df.groupby("item_id")["sales"].shift(1)
+    df["sales_last_year"] = df.groupby("item_id")["sales"].shift(12)
+
+    return df.set_index(["dates", "item_id"]).dropna()
+
+
+def build_model(config):
     if config["model"] == "PrevMonthSale":
-        df_sales["prediction"] = df_sales.groupby("item_id")["sales"].shift(1)
+        return PredictPrevMonthSale()
     elif config["model"] == "SameMonthLastYearSales":
-        df_sales["prediction"] = df_sales.groupby("item_id")["sales"].shift(12)
+        return PredictLastYearSale()
+    elif config["model"] == "Ridge":
+        return Ridge()
 
-    df_sales = df_sales[df_sales["dates"] >= config["start_test"]].reset_index(drop=True)
+    raise ValueError(f"Unknown model {config['model']}")
 
-    return df_sales[["dates", "item_id", "prediction"]]
+
+class PredictPrevMonthSale():
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X):
+        return X["sales_last_month"]
+
+
+
+class PredictLastYearSale():
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X):
+        return X["sales_last_year"]
